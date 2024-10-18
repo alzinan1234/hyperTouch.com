e581b59d4b0aba4431684d76d15f34e707564df2
Al-Zinan <pappurohan1100@gmail.com>
2024-10-18 16:08:04 +0600

    add

--- src/app/layout/MainLayout/Footer/Footer.jsx
+++ src/app/layout/MainLayout/Footer/Footer.jsx
@@ -95,6 +95,12 @@ const Footer = () => {
         <div className="text-center py-3">
           <p>&copy; 2022 All rights reserved.</p>
         </div>
+        <div className="text-center py-3">
+          <p>&copy; 2022 All rights reserved.</p>
+        </div>
+        <div className="text-center py-3">
+          <p>&copy; 2022 All rights reserved.</p>
+        </div>
       </footer>
     </>
   );
