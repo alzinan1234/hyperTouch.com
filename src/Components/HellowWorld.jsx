65473048cc046c58143883978ec4ef350e4b2070
Al-Zinan <pappurohan1100@gmail.com>
2024-11-13 18:51:29 +0600

    added

--- src/Components/HellowWorld.jsx
+++ src/Components/HellowWorld.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const HellowWorld = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1> hellow world </h1>
+      <h1> hellow world </h1>
+    </div>
+  );
 };
 
 export default HellowWorld;
