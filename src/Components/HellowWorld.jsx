abfbc1048b59220a4fbf01225eb94c86d9fc4f88
Al-Zinan <pappurohan1100@gmail.com>
2024-11-13 18:45:54 +0600

    added

--- src/Components/HellowWorld.jsx
+++ src/Components/HellowWorld.jsx
@@ -1,34 +1,7 @@
 import React from "react";
 
 const HellowWorld = () => {
-  return (
-    <div>
-      <h1>
-        hellow boy iam zinan my father name is md mofazzal hosian . my mother
-        name is farzana yesmin{" "}
-      </h1>
-      <h1>
-        hellow boy iam zinan my father name is md mofazzal hosian . my mother
-        name is farzana yesmin{"S "}
-      </h1>
-      <h1>
-        hellow boy iam zinan my father name is md mofazzal hosian . my mother
-        name is farzana yesmin{" "}
-      </h1>
-      <h1>
-        hellow boy iam zinan my father name is md mofazzal hosian . my mother
-        name is farzana yesmin{" "}
-      </h1>
-      <h1>
-        hellow boy iam zinan my father name is md mofazzal hosian . my mother
-        name is farzana yesmin{" "}
-      </h1>
-      <h1>
-        hellow boy iam zinan my father name is md mofazzal hosian . my mother
-        name is farzana yesmin{" "}
-      </h1>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default HellowWorld;
