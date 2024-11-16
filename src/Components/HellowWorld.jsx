8b1e668aa57ca2669fa9ee5ce8749930b907a866
Al-Zinan <pappurohan1100@gmail.com>
2024-11-16 21:28:19 +0600

    added

--- src/Components/HellowWorld.jsx
+++ src/Components/HellowWorld.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const HellowWorld = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>This is a simple React component.</p>
-      <p>Created by: Your Name</p>
+    
     </div>
   );
 };
