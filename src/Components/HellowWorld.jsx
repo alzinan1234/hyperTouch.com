4fce9e8d4394d4c7541567543023d840c04dec64
Al-Zinan <pappurohan1100@gmail.com>
2024-11-19 14:48:14 +0600

    added

--- src/Components/HellowWorld.jsx
+++ src/Components/HellowWorld.jsx
@@ -3,26 +3,7 @@ import React from "react";
 const HellowWorld = () => {
   return (
     <div>
-      <h1>Hello, World!</h1>
-      <p>This is a simple React component.</p>
-      <p>This component is created using ES6 syntax.</p>
-      <p>
-        The 'export default' statement is used to make the component available
-        for other files to import.
-      </p>
-      <p>
-        The 'export' statement is used to make individual components or
-        functions available for other files to import.
-      </p>
-      <p>
-        The 'import' statement is used to import components or functions from
-        other files.
-      </p>
-      <p>The 'React' library is imported at the top of the file.</p>
-      <p>
-        The 'h1', 'p', and 'div' elements are used to create the layout of the
-        component.
-      </p>
+ 
     </div>
   );
 };
